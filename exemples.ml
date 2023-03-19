57bb757b31a1d43e22fd9e1013b738516993aaa3
GioFieux <94927264+GioFieux@users.noreply.github.com>
2023-03-19 22:42:56 +0100

    update "Test.ml"

--- exemples.ml
+++ exemples.ml
@@ -1,4 +1,22 @@
 #use "Test.ml" ;;
+(* Test Supplémentaire*)
+(*let abs_property x = x >= 0;;
+let abs_property_bis x = x<0;;
+let abs_test = Test.make_test
+    (Generator.int (-20) 20)
+    (Reduction.int)
+    (abs_property);;
+
+let abs_test_bis = Test.make_test
+    (Generator.int (-20) 20)
+    (Reduction.int)
+    (abs_property_bis);;
+
+let result = Test.check 1000 abs_test;;
+let fail= Test.fails_at 1000 abs_test;;
+let execute = Test.execute 1000 [abs_test; abs_test_bis];;
+
+Printf.printf "Test result: %b\n" result;;*)
 
 (* Tests de la division euclidienne                                                                          *)
 (* Les tests sont effectués sur des couples d'entiers compris entre -100 et 100 dont le second est *non nul* *)
@@ -14,8 +32,8 @@ let red_intcouple =
 let test_intcouple = Test.make_test gen_intcouple red_intcouple ;;
 
 (* Construction des tests *)
-let test_quorem       = test_intcouple "/ et mod (correct)" (fun (a, b) -> (a = (a / b) * b + (a mod b))) ;;
-let test_quorem_wrong = test_intcouple "/ et mod (faux)"    (fun (a, b) -> (a = (a / b) * b - (a mod b))) ;;
+let test_quorem       = test_intcouple  (fun (a, b) -> (a = (a / b) * b + (a mod b))) ;;
+let test_quorem_wrong = test_intcouple    (fun (a, b) -> (a = (a / b) * b - (a mod b))) ;;
 
 (* Exécution des tests *)
 Test.check    100 test_quorem       ;;
@@ -35,8 +53,8 @@ let red_intlistcouple =
 let test_intlistcouple = Test.make_test gen_intlistcouple red_intlistcouple ;;
 
 (* Constructon des tests *)
-let test_append       = test_intlistcouple "List.@ (correct)" (fun (l1, l2) -> List.length (l1 @ l2) = (List.length l1) + (List.length l2)) ;;
-let test_append_wrong = test_intlistcouple "List.@ (faux)"    (fun (l1, l2) -> List.length (l1 @ l2) = (List.length l1) - (List.length l2)) ;;
+let test_append       = test_intlistcouple (fun (l1, l2) -> List.length (l1 @ l2) = (List.length l1) + (List.length l2)) ;;
+let test_append_wrong = test_intlistcouple (fun (l1, l2) -> List.length (l1 @ l2) = (List.length l1) - (List.length l2)) ;;
 
 (* Exécution des tests *)
 Test.check    100 test_append       ;;
@@ -44,4 +62,3 @@ Test.check    100 test_append_wrong ;;
 Test.fails_at 100 test_append       ;;
 Test.fails_at 100 test_append_wrong ;;
 Test.execute  100 [test_append ; test_append_wrong] ;;
-
