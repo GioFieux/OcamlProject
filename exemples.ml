7a82a0a8eb262867f23c91a024eee08ca70c2700
Nicolas LAMARQUE <100866077+Ashitaka06@users.noreply.github.com>
2023-03-24 10:37:50 +0100

    Ajout Test + MAJ visuelle
    Ajout du test de la fonctionnalité combine du module Property + Modifications visuelles pour rendre le code plus lisible

--- exemples.ml
+++ exemples.ml
@@ -1,6 +1,36 @@
 #use "Test.ml" ;;
+
+(*Test de la fonctionnalité supplémentaire combine du module Property*)
+(* Définir les propriétés *)
+(*
+let positive_sum_prop : (int * int) Property.t = fun (x, y) -> x + y > 0
+let even_sum_prop : (int * int) Property.t = fun (x, y) -> (x + y) mod 2 = 0
+(* Combiner les propriétés *)
+let combined_prop = Property.combine positive_sum_prop even_sum_prop
+(* Créer un générateur de paires d'entiers *)
+let int_gen = Generator.int (-100) 100
+let pair_gen = Generator.combine int_gen int_gen
+(* Définir une stratégie de réduction 
+let int_red = Reduction.int
+let pair_red = Reduction.combine int_red int_red *)
+(* Créer un test *)
+let test = Test.make_test pair_gen pair_red combined_prop
+(* Effectuer le test *)
+let n = 1  (* Nombre de valeurs à tester *)
+let result = Test.check n test
+(* Afficher le résultat *)
+let () = if result
+         then Printf.printf "Le test a réussi.\n"
+         else Printf.printf "Le test a échoué.\n" 
+*)
+
+
+(* ---------------------------------------------- *)
+
+
 (* Test Supplémentaire*)
-(*let abs_property x = x >= 0.;;
+(*
+let abs_property x = x >= 0.;;
 let abs_property_bis x = x<0.;;
 let abs_test = Test.make_test
     (Generator.float (-20.) 20.)
@@ -16,7 +46,12 @@ let result = Test.check 1000 abs_test;;
 let fail= Test.fails_at 1000 abs_test;;
 let execute = Test.execute 1000 [abs_test; abs_test_bis];;
 
-Printf.printf "Test result: %b\n" result;;*)
+Printf.printf "Test result: %b\n" result;;
+*)
+
+
+(* ---------------------------------------------- *)
+
 
 (* Tests de la division euclidienne                                                                          *)
 (* Les tests sont effectués sur des couples d'entiers compris entre -100 et 100 dont le second est *non nul* *)
@@ -44,7 +79,11 @@ Test.fails_at 100 test_quorem       ;;
 Test.fails_at 100 test_quorem_wrong ;;
 Test.execute  100 [test_quorem ; test_quorem_wrong] ;;
 
-(* Tests sur la concaténation de listes                                           *)
+
+(* ---------------------------------------------- *)
+
+
+(* Tests sur la concaténation de listes                                         *)
 (* Les tests sont effectués sur des listes d'au plus dix entiers entre 0 et 10. *)
 let gen_intlistcouple =
   let gen_intlist = Generator.list 10 (Generator.int_nonneg 10) in
