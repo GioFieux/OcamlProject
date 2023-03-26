77f21c15d76a2fc15e517f9af0d577d05671c9f7
LeoLAIOLO <96583365+LeoLAIOLO@users.noreply.github.com>
2023-03-26 20:29:59 +0200

    Déplacement des exemples

--- Generator.ml
+++ Generator.ml
@@ -229,19 +229,3 @@ module Generator :
     		;;
     
   end ;;
-
-(** Exemples d'utilisation**)
-let gen_int = Generator.int 1 30;;
-Generator.next gen_int;;
-
-let gen_intnonneg = Generator.int_nonneg 50;;
-Generator.next gen_intnonneg;;
-
-let gen_string = Generator.string 8 Generator.char;;
-Generator.next gen_string;;
-
-let gen_char = Generator.char;;
-Generator.next gen_char;;
-
-let gen_list = Generator.list 8 Generator.char;;
-Generator.next gen_list;;
