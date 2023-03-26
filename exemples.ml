5beb9e774c6898a77bea57e28d007c53de983462
Nicolas LAMARQUE <100866077+Ashitaka06@users.noreply.github.com>
2023-03-26 20:30:26 +0200

    Update exemples.ml

--- exemples.ml
+++ exemples.ml
@@ -1,58 +1,5 @@
 #use "Test.ml" ;;
 
-(*Test de la fonctionnalité supplémentaire combine du module Property*)
-(* Définir les propriétés *)
-(*
-let positive_sum_prop : (int * int) Property.t = fun (x, y) -> x + y > 0
-let even_sum_prop : (int * int) Property.t = fun (x, y) -> (x + y) mod 2 = 0
-(* Combiner les propriétés *)
-let combined_prop = Property.combine positive_sum_prop even_sum_prop
-(* Créer un générateur de paires d'entiers *)
-let int_gen = Generator.int (-100) 100
-let pair_gen = Generator.combine int_gen int_gen
-(* Définir une stratégie de réduction 
-let int_red = Reduction.int
-let pair_red = Reduction.combine int_red int_red *)
-(* Créer un test *)
-let test = Test.make_test pair_gen pair_red combined_prop
-(* Effectuer le test *)
-let n = 1  (* Nombre de valeurs à tester *)
-let result = Test.check n test
-(* Afficher le résultat *)
-let () = if result
-         then Printf.printf "Le test a réussi.\n"
-         else Printf.printf "Le test a échoué.\n" 
-*)
-
-
-(* ---------------------------------------------- *)
-
-
-(* Test Supplémentaire*)
-(*
-let abs_property x = x >= 0.;;
-let abs_property_bis x = x<0.;;
-let abs_test = Test.make_test
-    (Generator.float (-20.) 20.)
-    (Reduction.float)
-    (abs_property);;
-
-let abs_test_bis = Test.make_test
-    (Generator.float (-20.) 20.)
-    (Reduction.float)
-    (abs_property_bis);;
-
-let result = Test.check 1000 abs_test;;
-let fail= Test.fails_at 1000 abs_test;;
-let execute = Test.execute 1000 [abs_test; abs_test_bis];;
-
-Printf.printf "Test result: %b\n" result;;
-*)
-
-
-(* ---------------------------------------------- *)
-
-
 (* Tests de la division euclidienne                                                                          *)
 (* Les tests sont effectués sur des couples d'entiers compris entre -100 et 100 dont le second est *non nul* *)
 (* (d'où l'utilisation du filtre pour éviter des divisions par zéro).                                        *)
