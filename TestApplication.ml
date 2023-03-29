9b26cd550f7ff80d7786979d873f8f6caf58a8ab
Nicolas LAMARQUE <100866077+Ashitaka06@users.noreply.github.com>
2023-03-29 21:41:15 +0200

    Update TestApplication.ml

--- TestApplication.ml
+++ TestApplication.ml
@@ -4,6 +4,7 @@
 
 let abs_property x = x >= 0.;;
 let abs_property_bis x = x<0.;;
+
 let abs_test = Test.make_test
     (Generator.float (-20.) 20.)
     (Reduction.float)
@@ -12,6 +13,13 @@ let abs_test_bis = Test.make_test
     (Generator.float (-20.) 20.)
     (Reduction.float)
     (abs_property_bis);;
+    
 let result = Test.check 1000 abs_test;;
 let fail= Test.fails_at 1000 abs_test;;
 let execute = Test.execute 1000 [abs_test; abs_test_bis];;
+
+let combined_test = Test.combine_tests abs_test abs_test_bis ;;
+
+let combined_result = Test.check 1000 combined_test ;;
+let combined_fail = Test.fails_at 1000 combined_test ;;
+let combined_execute = Test.execute 1000 [abs_test; abs_test_bis; combined_test] ;;
