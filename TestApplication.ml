11a1677c42ad9581e43be3165568d1c982a945a5
Nicolas LAMARQUE <100866077+Ashitaka06@users.noreply.github.com>
2023-03-29 18:43:17 +0200

    Update TestApplication.ml

--- TestApplication.ml
+++ TestApplication.ml
@@ -15,4 +15,5 @@ let abs_test_bis = Test.make_test
 let result = Test.check 1000 abs_test;;
 let fail= Test.fails_at 1000 abs_test;;
 let execute = Test.execute 1000 [abs_test; abs_test_bis];;
-Printf.printf "Test result: %b\n" result;;
+let report = Test.generate_report 100 test_list;;
+print_string report;;
